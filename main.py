0db1f82efeedb67943349e1d674b1fb1ddc8d83d
Leilani Dore <ldore@brooklynsteamcenter.org>
2024-12-10 10:48:01 -0500

    need to make browse page media responsive

--- main.py
+++ main.py
@@ -1,7 +1,39 @@
 from flask import Flask, render_template
+import pymysql
+from dynaconf import Dynaconf
+
 
 app = Flask(__name__)
+
+conf = Dynaconf(
+    settings_file = ['settings.toml']
+)
+
+def  connect_dv():
+    conn =pymysql.connect(
+        host="10.100.34.80",
+        database="ldore_buy_a_rock",
+        user = "ldore",
+        password = conf.password,
+        autocommit= True,
+        cursorclass= pymysql.cursors.DictCursor,
+
+    )
+    return conn
 
 @app.route("/")
 def index():
     return render_template("homepage.html.jinja")
+
+
+@app.route("/browse")
+def  product_browse ():
+    conn = connect_dv()
+
+    cursor = conn.cursor()
+
+    cursor.execute("SELECT * FROM `Product` ; ")
+
+    results = cursor.fetchall()
+
+    return render_template("browse.html.jinja", product = results)
