6fa81eaa3eef0ce15ae3a550806dd46bc8ca684c
Leilani Dore <ldore@brooklynsteamcenter.org>
2025-01-02 10:51:25 -0500

    edited cart

--- main.py
+++ main.py
@@ -226,5 +226,29 @@ def logout():
 @app.route('/cart')
 @flask_login.login_required
 def cart():
-   return "hi"
+   conn=connect_db()
+   cursor= conn.cursor()
+
+   customer_id = flask_login.current_user.user_id
+   cursor.execute(f"""
+                SELECT 
+                    `product_name`,
+                    `price`,
+                    `Cart`.`quantity`,
+                    `image`,
+                    `product_id`,
+                    `Cart`.`id`
+                FROM `Cart` 
+                JOIN `Product` ON `product_id` = `Product`.`id` 
+                WHERE `customer_id` =  {customer_id};""")
+
+   results = cursor.fetchall()
+
+   cursor.close()
+
+   conn.close()
+
+
+
+   return render_template('cart.html.jinja', products=results)
     
